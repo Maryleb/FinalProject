6865254c4f85b668dd4a19b7048e807e8f36e610
Maryleb <maryleb2012@gmail.com>
2024-01-18 13:18:50 +1000

    Исправлена запись клиентов

--- project/controllers/record.py
+++ project/controllers/record.py
@@ -2,28 +2,42 @@ from app import app
 from flask import render_template, request, session
 #import sqlite3
 from utils import get_db_connection
-from models.record_model import get_master_available, get_date_available, get_master_time
+from models.record_model import get_master, get_free_record, get_free_record_all_masters, add_record, get_client
 @app.route('/record')
 def record():
     conn = get_db_connection()
 
-    # нажата кнопка Показать
-    if request.values.get('day_begin'):
-        date_s = request.values.get('day_begin')
-        session['date_s'] = date_s
+    if request.values.get('master') and request.values.get('date_start') and request.values.get('date_end'):
+        master = int(request.values.get('master'))
+        date_start = request.values.get('date_start')
+        date_end = request.values.get('date_end')
     else:
-        session['date_s'] = '2023-11-01'
-    df_master_available = get_master_available(conn, session['date_s'])
-    df_date_available = get_date_available(conn, session['date_s'])
-    df_master_time = get_master_time(conn, session['date_s'])
+        master = -1
+        date_start = '2023-11-01'
+        date_end = '2023-11-02'
+
+    if master == 0:
+        df_free_record = get_free_record_all_masters(conn, date_start, date_end)
+    else:
+        df_free_record = get_free_record(conn, master, date_start, date_end)
+    df_master = get_master(conn)
+    df_client = get_client(conn)
+
+    if request.values.get('data') and request.values.get('pet_name') and request.values.get('client'):
+        data = request.values.get('data')
+        record_date = data[:10]
+        record_time = data[10:18]
+        master_name = data[18:]
+        client_id = int(request.values.get('client'))
+        pet_name = request.values.get('pet_name')
+        add_record(conn, client_id, record_date, record_time, pet_name, master_name)
 
      # выводим форму
     html = render_template(
      'record.html',
-     date_s = session['date_s'],
-     master_available=df_master_available,
-     date_available=df_date_available,
-     master_time = df_master_time,
+     combo_box = df_master,
+     relation_free = df_free_record,
+     combo_box1 = df_client,
      len = len
     )
     return html
--- project/models/record_model.py
+++ project/models/record_model.py
@@ -1,45 +1,63 @@
 import pandas
-def get_master_available(conn, date_start):
+
+def get_master(conn):
  return pandas.read_sql(
  '''
-    SELECT DISTINCT master_full_name, record_date, record_time
-    FROM master INNER JOIN timetable USING (master_id)
-    INNER JOIN timetable_date USING (timetable_id)
-    INNER JOIN record USING (timetable_date_id)
-    LEFT OUTER JOIN pet USING (pet_id)
-    WHERE pet_id IS NULL AND record_date >= :date_s AND record_date < date(:date_s, "+8 days")
-    ORDER BY master_full_name, record_date, record_time;
- ''', conn, params={"date_s": date_start})
+  SELECT * FROM master
+ ''', conn)
 
-def get_date_available(conn, date_start):
+def get_client(conn):
  return pandas.read_sql(
  '''
-    SELECT DISTINCT record_date, 
-    CASE
-     WHEN strftime('%w',record_date) == '1' THEN "Понедельник"
-     WHEN strftime('%w',record_date) == '2' THEN "Вторник"
-     WHEN strftime('%w',record_date) == '3' THEN "Среда"
-     WHEN strftime('%w',record_date) == '4' THEN "Четверг"
-     WHEN strftime('%w',record_date) == '5' THEN "Пятница"
-     WHEN strftime('%w',record_date) == '6' THEN "Суббота"
-     WHEN strftime('%w',record_date) == '7' THEN "Воскресенье"
-     ELSE "Не существует"
-    END AS weekday
-    FROM master INNER JOIN timetable USING (master_id)
-    INNER JOIN timetable_date USING (timetable_id)
-    INNER JOIN record USING (timetable_date_id)
-    LEFT OUTER JOIN pet USING (pet_id)
-    WHERE pet_id IS NULL AND record_date >= :date_s AND record_date < date(:date_s, "+8 days")
-    ORDER BY record_date;
- ''', conn, params={"date_s": date_start})
+  SELECT * FROM client
+  ORDER BY client_name
+ ''', conn)
 
-def get_master_time(conn, date_start):
+def get_free_record_all_masters(conn, date_start, date_end):
  return pandas.read_sql(
-'''  SELECT master_full_name, record_date, record_time
+ f'''
+  SELECT master_full_name, master_id, record_date, record_time
   FROM master INNER JOIN timetable USING (master_id)
   INNER JOIN timetable_date USING (timetable_id)
-  INNER JOIN record USING (timetable_date_id)
+  LEFT OUTER JOIN record USING (timetable_date_id)
   LEFT OUTER JOIN pet USING (pet_id)
-  WHERE pet_id IS NULL AND record_date >= :date_s AND record_date < date(:date_s, "+7 days")
-  ORDER BY record_date;
-  ''', conn, params={"date_s": date_start})
+  WHERE record.pet_id IS NULL AND record_date >= '{date_start}' AND record_date <= '{date_end}'
+ ''', conn)
+
+def get_free_record(conn, master_id, date_start, date_end):
+ return pandas.read_sql(
+ f'''
+  SELECT master_full_name, master_id, record_date, record_time
+  FROM master INNER JOIN timetable USING (master_id)
+  INNER JOIN timetable_date USING (timetable_id)
+  LEFT OUTER JOIN record USING (timetable_date_id)
+  LEFT OUTER JOIN pet USING (pet_id)
+  WHERE record.pet_id IS NULL AND record_date >= '{date_start}' AND record_date <= '{date_end}' AND master_id == {master_id}
+ ''', conn)
+
+def execute_query(connection, query):
+ cursor = connection.execute(query)
+ try:
+  cursor.execute(query)
+  connection.commit()
+  print("Query executed successfuly")
+ except Error as e:
+  print(f"The error '{e}' occured")
+
+
+def add_record(conn, client_id, record_date, record_time, pet_name, master):
+ added = f'''
+   UPDATE record SET pet_id = (
+   SELECT pet_id
+   FROM pet INNER JOIN client USING(client_id)
+   WHERE client_id == {client_id} and pet_name == '{pet_name}'
+  )
+  WHERE record_time == '{record_time}' AND timetable_date_id == (
+   SELECT timetable_date_id 
+   FROM master INNER JOIN timetable USING (master_id)
+   INNER JOIN timetable_date USING (timetable_id)
+   INNER JOIN record USING (timetable_date_id)
+   WHERE record_date == '{record_date}' AND record_time == '{record_time}' AND master_full_name LIKE '%{master}%'
+   LIMIT 1)
+ '''
+ execute_query(conn, added)
